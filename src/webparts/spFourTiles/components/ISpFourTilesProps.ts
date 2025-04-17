77d5a4a192626c8172a89ccb90291ce2deac15eb
Bokiej <bokiejp1@emea.roche.com>
2025-04-17 21:58:13 +0200

    template

---
+++ src/webparts/spFourTiles/components/ISpFourTilesProps.ts
@@ -0,0 +1,7 @@
+export interface ISpFourTilesProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/spFourTiles/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpFourTilesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpFourTilesWebPartStrings' {
+  const strings: ISpFourTilesWebPartStrings;
+  export = strings;
+}
